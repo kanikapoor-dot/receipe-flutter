70098f6341576e1475662ed277406e0f64200247
kani kapoor <kanikapoor24@gmail.com>
2020-08-09 14:25:47 +0530

    text field

--- lib/screens/home_screeen.dart
+++ lib/screens/home_screeen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +9,79 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'EatNew',
+          style: TextStyle(
+              color: Colors.lightBlue,
+              fontFamily: 'Architects_Daughter',
+              fontSize: 25.0),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(5.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                  child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Material(
+                      elevation: 5.0,
+                      shadowColor: Colors.white,
+                      child: TextField(
+                        textCapitalization: TextCapitalization.sentences,
+                        keyboardType: TextInputType.text,
+                        onChanged: (vl) {
+                          print(vl);
+                        },
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                        cursorColor: Colors.lightBlue,
+                        autocorrect: true,
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.room_service,
+                            color: Colors.lightBlue,
+                          ),
+                          border: InputBorder.none,
+//                          focusedBorder: InputBorder.none,
+//                          enabledBorder: InputBorder.none,
+//                          disabledBorder: InputBorder.none,
+//                          errorBorder: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Search Recipes',
+                        style: TextStyle(
+                          fontFamily: 'Architects_Daughter',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 17.0,
+                        ),
+                      ),
+                      color: Colors.lightBlue,
+                      textColor: Colors.white,
+                    ),
+                  ],
+                ),
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
