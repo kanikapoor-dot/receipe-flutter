0241fc1ef0e74a949d0c533c7222fb87762cb089
kani kapoor <kanikapoor24@gmail.com>
2020-08-10 11:10:19 +0530

    softUI

--- lib/screens/home_screeen.dart
+++ lib/screens/home_screeen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:receipe/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,78 +11,93 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(
-          'EatNew',
-          style: TextStyle(
-              color: Colors.lightBlue,
-              fontFamily: 'Architects_Daughter',
-              fontSize: 25.0),
-        ),
-        backgroundColor: Colors.white,
-      ),
+      resizeToAvoidBottomPadding: false,
+      backgroundColor: kCyanColor,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(5.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                  child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Material(
-                      elevation: 5.0,
-                      shadowColor: Colors.white,
-                      child: TextField(
-                        textCapitalization: TextCapitalization.sentences,
-                        keyboardType: TextInputType.text,
-                        onChanged: (vl) {
-                          print(vl);
-                        },
-                        style: TextStyle(
-                          fontSize: 20.0,
-                        ),
-                        cursorColor: Colors.lightBlue,
-                        autocorrect: true,
-                        autofocus: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.room_service,
-                            color: Colors.lightBlue,
-                          ),
-                          border: InputBorder.none,
-//                          focusedBorder: InputBorder.none,
-//                          enabledBorder: InputBorder.none,
-//                          disabledBorder: InputBorder.none,
-//                          errorBorder: InputBorder.none,
-                        ),
-                      ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: RecipeSearch(),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(100.0),
                     ),
-                    FlatButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Search Recipes',
-                        style: TextStyle(
-                          fontFamily: 'Architects_Daughter',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 17.0,
-                        ),
-                      ),
-                      color: Colors.lightBlue,
-                      textColor: Colors.white,
+                  ),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 25.0, horizontal: 15.0),
+                    child: ListView(
+                      children: <Widget>[],
                     ),
-                  ],
+                  ),
                 ),
-              )),
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class RecipeSearch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(0.0),
+            decoration: BoxDecoration(
+              color: kCyanColor,
+              boxShadow: kSearchBoxShadow,
+              borderRadius: BorderRadius.circular(100.0),
+            ),
+            child: TextField(
+              textAlign: TextAlign.center,
+              cursorColor: Colors.white,
+              textCapitalization: TextCapitalization.sentences,
+              style: TextStyle(
+                fontFamily: kFontArch,
+                fontWeight: FontWeight.w700,
+                letterSpacing: 1.0,
+              ),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Eg.chicken',
+                contentPadding: EdgeInsets.symmetric(horizontal: 10.0)
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            padding: EdgeInsets.all(0.0),
+            decoration: BoxDecoration(
+              color: kCyanColor,
+              boxShadow: kSearchBoxShadow,
+              borderRadius: BorderRadius.circular(100.0),
+            ),
+            child: FlatButton(
+              onPressed: (){},
+              child: Text(
+                'Search',
+                style: TextStyle(
+                  fontFamily: kFontArch,
+                ),
+              ),
+            ),
+          ),
+        ],
     );
   }
 }
